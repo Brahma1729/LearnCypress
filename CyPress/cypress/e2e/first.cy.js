8ff4c734f7f338fc2628e3be9fd008754acb509f
Brahma1729 <malluri401@outlook.com>
2023-08-27 12:54:02 -0400

    repo clean up

--- CyPress/cypress/e2e/first.cy.js
+++
@@ -1,8 +0,0 @@
-describe('This is my first test suite', () => {
-    
-    it('This is my first test case', () => {
-        
-        expect(true).to.equal(true)
-        //expect(true).to.equal(false)
-    });
-});
